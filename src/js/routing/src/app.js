6fd64a1dfcf238ba2a9b8aca5b92fb1e28b883df
Nagarajan, Satheesh (ESI) <satheesh.nagarajan@adp.com>
2016-06-27 20:00:45 +0530

    routing app and sample client side script for getting execution metrics

---
+++ src/js/routing/src/app.js
@@ -0,0 +1,29 @@
+
+var express = require('express');
+var util = require('util')
+var app = express();
+var reqCount = 0;
+
+
+app.get('/', function(req, res) {
+
+  var looping = function looping() {
+
+    for (var index = 0; i<100; index++){
+      console.log(index);
+    }
+
+  }
+
+  console.log(looping());
+
+  res.status(200).send({"result":"Function Executed"});
+  console.log(++reqCount);
+});
+app.listen(3000, function () {
+  console.log('listening on port 3000!');
+});
+
+process.on('uncaughtException', function(err) {
+  console.info(util.inspect(err, {colors: true}));
+});
